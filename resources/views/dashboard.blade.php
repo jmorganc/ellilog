abb9eff3f9588fdfbf01ca05191c6d08a3335260
Morgan Campbell <morgan.campbell@utdallas.edu>
2015-09-11 10:47:42 -0500

    View testing

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ellilog - Dashboard</title>
+    </head>
+    <body>
+        <h1>Ellilog</h1>
+        <h2>Dashboard</h2>
+        <h3>Welcome</h3>
+        <p>Dashboard: <?php echo $data; ?></p>
+    </body>
+</html>
