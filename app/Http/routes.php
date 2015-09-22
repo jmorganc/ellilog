8999915e78b0a18993261934266d2db87928b70e
Morgan Campbell <jmorgancampbell@gmail.com>
2015-09-21 21:52:39 -0500

    /log

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,27 @@
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return view('dashboard', ['data' => 'Here\'s some welcome data.']);
+    return view('welcome');
+});
+
+
+Route::get('/log', function () {
+    $res = Requests::get('http://api.ellilog.com/api/v0/users?active=1', array('Accept' => 'application/json'));
+    $users = json_decode($res->body);
+    $users = $users->data;
+
+    $res = Requests::get('http://api.ellilog.com/api/v0/babies?active=1&id=1', array('Accept' => 'application/json'));
+    $babies = json_decode($res->body);
+    $babies = $babies->data;
+
+    $res = Requests::get('http://api.ellilog.com/api/v0/things?active=1', array('Accept' => 'application/json'));
+    $things = json_decode($res->body);
+    $things = $things->data;
+
+    return view('dashboard', [
+        'data' => 'Here\'s some welcome data.',
+        'users' => $users,
+        'babies' => $babies,
+        'things' => $things
+    ]);
 });
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,5 +8,25 @@
         <h2>Dashboard</h2>
         <h3>Welcome</h3>
         <p>Dashboard: <?php echo $data; ?></p>
+
+        <form action="/log" method="POST">
+            User: <select id="user" name="user">
+                <?php foreach($users as $user) { ?>
+                <option value="<?php echo $user->id; ?>"><?php echo $user->first_name . ' ' . $user->last_name; ?></option>
+                <?php } ?>
+            </select>
+
+            Baby: <select id="baby" name="baby">
+                <?php foreach($babies as $baby) { ?>
+                <option value="<?php echo $baby->id; ?>"><?php echo $baby->first_name . ' ' . $baby->last_name; ?></option>
+                <?php } ?>
+            </select>
+
+            Thing: <select id="thing" name="thing">
+                <?php foreach($things as $thing) { ?>
+                <option value="<?php echo $thing->thing; ?>"><?php echo $thing->thing; ?></option>
+                <?php } ?>
+            </select>
+        </form>
     </body>
 </html>
