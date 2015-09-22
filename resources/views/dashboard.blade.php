abc75dfdde465d0480886d97f001a9d136da4b32
Morgan Campbell <jmorgancampbell@gmail.com>
2015-09-21 23:44:13 -0500

    Bootstrap theme

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,46 +1,53 @@
 @extends('layouts.master')
 @section('title', 'Dashboard')
 @section('content')
-        <h3>Log</h3>
+        <div class="row marketing">
+            <div class="col-lg-6">
+                <h4>Log</h4>
+                <form action="/log" method="POST">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <p>
+                    User: <select id="user_id" name="user_id">
+                        <?php foreach($users as $user) { ?>
+                        <option value="{{$user->id}}">{{$user->first_name}} {{$user->last_name}}</option>
+                        <?php } ?>
+                    </select>
+                    </p>
 
-        <form action="/log" method="POST">
-            <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <p>
-            User: <select id="user_id" name="user_id">
-                <?php foreach($users as $user) { ?>
-                <option value="{{$user->id}}">{{$user->first_name}} {{$user->last_name}}</option>
-                <?php } ?>
-            </select>
-            </p>
+                    <p>
+                    Baby: <select id="baby_id" name="baby_id">
+                        <?php foreach($babies as $baby) { ?>
+                        <option value="{{$baby->id}}">{{$baby->first_name}} {{$baby->last_name}}</option>
+                        <?php } ?>
+                    </select>
+                    </p>
 
-            <p>
-            Baby: <select id="baby_id" name="baby_id">
-                <?php foreach($babies as $baby) { ?>
-                <option value="{{$baby->id}}">{{$baby->first_name}} {{$baby->last_name}}</option>
-                <?php } ?>
-            </select>
-            </p>
+                    <p>
+                    Thing: <select id="thing_id" name="thing_id">
+                        <?php foreach($things as $thing) { ?>
+                        <option value="{{$thing->thing}}">{{$thing->thing}}</option>
+                        <?php } ?>
+                    </select>
+                    </p>
 
-            <p>
-            Thing: <select id="thing_id" name="thing_id">
-                <?php foreach($things as $thing) { ?>
-                <option value="{{$thing->thing}}">{{$thing->thing}}</option>
-                <?php } ?>
-            </select>
-            </p>
+                    <p>
+                    Ounces: <select id="data_milk" name="data_milk">
+                        <?php foreach(range(1, 10) as $oz) {
+                            echo '<option value="' . $oz . '">' . $oz . '</option>';
+                        } ?>
+                    </select>
+                    </p>
 
-            <p>
-            Ounces: <select id="data_milk" name="data_milk">
-                <?php foreach(range(1, 10) as $oz) {
-                    echo '<option value="' . $oz . '">' . $oz . '</option>';
-                } ?>
-            </select>
-            </p>
+                    <p>Notes:<br/>
+                    <textarea id="notes" name="notes"></textarea>
+                    </p>
 
-            <p>Notes:<br/>
-            <textarea id="notes" name="notes"></textarea>
-            </p>
+                    <p><input type="submit" name="submit" value="Submit" /></p>
+                </form>
+            </div>
 
-            <p><input type="submit" name="submit" value="Submit" /></p>
-        </form>
+            <!-- <div class="col-lg-6">
+                Other side
+            </div> -->
+        </div>
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,10 +2,47 @@
 <html>
     <head>
         <title>Ellilog - @yield('title')</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
-        <h1>Ellilog</h1>
-        <h2>@yield('title')</h2>
-        @yield('content')
+        <div class="container">
+
+            <div class="header clearfix">
+                <nav>
+                    <ul class="nav nav-pills pull-right">
+                        <li role="presentation" class="active"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">About</a></li>
+                        <li role="presentation"><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <h3 class="text-muted">Ellilog</h3>
+            </div>
+
+            <!-- <div class="jumbotron">
+                <h1>Jumbotron heading</h1>
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+            </div> -->
+
+            @yield('content')
+            <footer class="footer">
+                <!-- <p>&copy; Company 2014</p> -->
+                <p>Email any problems to: <a href="mailto:jmorgancampbell@gmail.com?suject=Ellilog Feedback">jmorgancampbell@gmail.com</a></p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     </body>
 </html>
