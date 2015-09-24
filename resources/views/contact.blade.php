7e12d856d95eb7cf36f2052b92e9a9f0e6830633
CraigKimbia <craig@kimbia.com>
2015-09-23 21:28:08 -0500

    Create PagesController for about and contact pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+@section('title', 'About')
+@section('content')
+        <div class="row marketing">
+            <div class="col-lg-12">
+                <h3>Contact</h3>
+                <p>Contact that stud <a href="mailto:jmorgancampbell@gmail.com?suject=Ellilog Feedback">jmorgancampbell@gmail.com</a> if you need anything specific. I have no ownership or shares invested in these babies, so he's the one you want to talk to.</p>
+            </div>
+
+            <!-- <div class="col-lg-6">
+                Other side
+            </div> -->
+        </div>
+@stop
