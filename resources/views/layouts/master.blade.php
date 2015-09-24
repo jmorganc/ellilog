13148330caab16b257fee2a9fa5745adf4d2dfe5
CraigKimbia <craig@kimbia.com>
2015-09-23 21:27:47 -0500

    Update navigation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,8 +22,8 @@
                 <nav>
                     <ul class="nav nav-pills pull-right">
                         <li role="presentation" class="active"><a href="#">Home</a></li>
-                        <li role="presentation"><a href="#">About</a></li>
-                        <li role="presentation"><a href="#">Contact</a></li>
+                        <li role="presentation"><a href="{{ url('about') }}">About</a></li>
+                        <li role="presentation"><a href="{{ url('contact') }}">Contact</a></li>
                     </ul>
                 </nav>
                 <h3 class="text-muted">Ellilog</h3>
